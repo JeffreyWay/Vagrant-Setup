3c91d00cb5d92865476fccc7e195c22143fb12bc
Jeffrey Way <jeffrey@jeffrey-way.com>
2013-10-30 17:03:39 -0400

    Allow override

--- install.sh
+++ install.sh
@@ -45,6 +45,8 @@ echo "--- What developer codes without errors turned on? Not you, master. ---"
 sed -i "s/error_reporting = .*/error_reporting = E_ALL/" /etc/php5/apache2/php.ini
 sed -i "s/display_errors = .*/display_errors = On/" /etc/php5/apache2/php.ini
 
+sed -i 's/AllowOverride None/AllowOverride All/' /etc/apache2/apache2.conf
+
 echo "--- Restarting Apache ---"
 sudo service apache2 restart
 
